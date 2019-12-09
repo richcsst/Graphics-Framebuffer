5ece1538101f26e50d2d5cc9a7ffee551c6b3820
Richard Kelsch <rich@csst.net>
2019-12-09 11:40:26 -0700

    Added prerequisite installers

---
+++ install-prerequisites-debian.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install build-essential \
+libimager-perl \
+libinline-c-perl \
+libmath-gradient-perl \
+libmath-bezier-perl \
+libfile-map-perl \
+libtest-most-perl \
+libsys-cpu-perl \
+fonts-wine
---
+++ install-prerequisites-redhat.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo yum update
+sudo yum upgrade kernel-headers \
+build-essential \
+perl-math-gradient \
+perl-math-bezier \
+perl-file-map \
+perl-imager \
+perl-inline-c \
+perl-sys-cpu \
+perl-test-most
