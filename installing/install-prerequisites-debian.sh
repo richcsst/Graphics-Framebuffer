8c429d63716dde53e9937a11e410d569a5e44ae0
Richard Kelsch <rich@csst.net>
2023-10-03 06:22:58 -0600

    Install without prompting in apt

--- installing/install-prerequisites-debian.sh
+++ installing/install-prerequisites-debian.sh
@@ -6,25 +6,25 @@ sudo apt update # Bring the Debian module database up to date
 
 # Absolutely needed
 
-sudo apt install build-essential \
-                 libfreetype6-dev \
-                 libgif-dev \
-                 libjpeg-dev \
-                 libpng-dev \
-                 libtiff-dev \
-                 libfreetype6-dev \
-                 fonts-wine
+sudo apt install -y build-essential \
+                    libfreetype6-dev \
+                    libgif-dev \
+                    libjpeg-dev \
+                    libpng-dev \
+                    libtiff-dev \
+                    libfreetype6-dev \
+                    fonts-wine
 
 # Only needed if you are using the OS installed (packaged) Perl
-read -p "Do you wish to install the packaged/system Perl prerequisites?" yn
+read -p "Do you wish to install the packaged/system Perl prerequisites?  " yn
 case $yn in
     [Yy]* ) 
-        sudo apt install libimager-perl \
-                         libinline-c-perl \
-                         libmath-gradient-perl \
-                         libmath-bezier-perl \
-                         libfile-map-perl \
-                         libtest-most-perl \
-                         libterm-readkey-perl \
-                         libsys-cpu-perl;;
+        sudo apt install -y libimager-perl \
+                            libinline-c-perl \
+                            libmath-gradient-perl \
+                            libmath-bezier-perl \
+                            libfile-map-perl \
+                            libtest-most-perl \
+                            libterm-readkey-perl \
+                            libsys-cpu-perl;;
 esac
