441935808647562de4ce50daa80c3c7ff8870e39
Richard Kelsch <richcsst@users.noreply.github.com>
2026-01-26 21:07:25 -0700

    Update echo messages for clarity in script

--- installing/install-prerequisites-debian.sh
+++ installing/install-prerequisites-debian.sh
@@ -17,9 +17,9 @@ sudo apt install -y build-essential \
                     fonts-wine
 
 # Only needed if you are using the OS installed (packaged) Perl
-echo "OS prerequisites installed.  It is recommended you answer YES to the next question."
+echo "OS prerequisites installed.  Now for the Perl prerequisites."
+echo "It is recommended you answer YES to the next question:"
 
-read -p "Do you wish to install the packaged/system Perl module prerequisites?  " yn
 case $yn in
     [Yy]* ) 
         sudo apt install -y libimager-perl \
