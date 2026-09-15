0043f29b7c6ff8045729e8f6e825f6ad359e126a
Richard Kelsch <richcsst@users.noreply.github.com>
2026-09-15 12:37:21 -0600

    Still disappointment

--- bin/gfb_viewer.pl
+++ bin/gfb_viewer.pl
@@ -35,7 +35,7 @@ if ($res_param =~ /^(\d+)x(\d+)(?:x(\d+))?$/i) {
 # Determine matching raw video pixel format for ffplay / mplayer
 my $pix_fmt;
 if ($bpp == 32) {
-    $pix_fmt = 'bgr0';
+    $pix_fmt = 'bgra';
 } elsif ($bpp == 24) {
     $pix_fmt = 'bgr24';
 } elsif ($bpp == 16) {
