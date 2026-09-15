0a5e25e13141f33b388ecb547c614b3a38d62df7
Richard Kelsch <richcsst@users.noreply.github.com>
2026-09-15 11:46:51 -0600

    Fixed again

--- examples/primitives.pl
+++ examples/primitives.pl
@@ -377,7 +377,7 @@ undef($F);             # Destroy the framebuffer object
 exit(0);
 
 sub vbox_flush {
-    if ($F->{'VBOX'} and ($F->{'LAST_FLUSHED'} + $interval) <= time and ! $F->{'GFB_VIEWER'})) { # Only flush if in a real console, not the GUI
+    if ($F->{'VBOX'} and ($F->{'LAST_FLUSHED'} + $interval) <= time and $F->{'GFB_VIEWER'} == FALSE) { # Only flush if in a real console, not the GUI
         $F->_flush_screen();
     }
 }
