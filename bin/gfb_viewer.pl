e37294a7e8ea12ba9a46f2edb2f8d172a78f0180
Richard Kelsch <richcsst@users.noreply.github.com>
2026-09-15 13:12:04 -0600

    Try again

--- bin/gfb_viewer.pl
+++ bin/gfb_viewer.pl
@@ -71,8 +71,8 @@ print "[gfb_viewer] Config:      $INFO_FILE ($width $height $bpp)\n";
 # Using ffplay (or mplayer fallback) configured for continuous raw video stream
 my @cmd = (
     'ffplay',
-    '-loglevel', 'quiet',
-    '-stats', '0',
+#    '-loglevel', 'quiet',
+#    '-stats', '0',
     '-f', 'rawvideo',
     '-pixel_format', $pix_fmt,
     '-video_size', "${width}x${height}",
